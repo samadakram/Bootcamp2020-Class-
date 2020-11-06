5c52c8e4de092df0bb1f6c653be6112636faca2f
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-07 02:18:40 +0500

    Update Room.js
    Added State by Hook in Room.js

--- src/Room.js
+++ src/Room.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 function Room(){
+
+    const [isLit, setLit] = React.useState(false);
+
     return(
         <div>
-            The room is lit
+            The room is {isLit ? 'lit' : 'dark'}
         </div>
     );
 }

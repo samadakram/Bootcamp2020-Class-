45d3b867da5825093c570f1d237617fba9b4e146
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-07 03:57:17 +0500

    Update App.js
    Added two buttons Named On and Off and the function of these buttons is change the state of component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 
 function App(){
 
-  let [isOn, setOn] = useState(false);
+  let [isOn, setOn] = useState(true);
 
   return(
     <div>
-      <Room light={isOn ? 'ON' : 'OFF'} />
+      <Room light={isOn ? 'ON' : 'OFF'}/>
+      <br/>
+      <button onClick={()=> setOn(true)}>ON</button>
+      <button onClick={()=> setOn(false)}>OFF</button>
     </div>
   );
 }
